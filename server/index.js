f68c2f2b1db7c825237ed4ac815b14e666996575
Coby Philip <cobyphilip577@gmail.com>
2026-04-17 22:33:15 +0800

    feat: completed project setup with server, repo,  and folder structure

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('This is the backend communicating!');
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+})
